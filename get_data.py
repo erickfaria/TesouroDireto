677db740b937799d988e4bc345b8471f90d5fe30
erickfaria <52634714+erickfaria@users.noreply.github.com>
2024-05-22 22:37:56 -0300

    chore: Refactor get_data.py and tesouro_direto.ipynb

--- get_data.py
+++ get_data.py
@@ -1,112 +1,87 @@
 import pandas as pd
+import requests
+import os
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 class TesouroDiretoDatasets:
     """
-    A class for accessing and reading datasets from the Brazilian Treasury Direct (Tesouro Direto).
+    A class for downloading and saving datasets from the Brazilian Treasury Direct (Tesouro Direto).
 
-    This class provides methods to load data from the Tesouro Direto directly from URLs
-    provided by Tesouro Transparente, eliminating the need to manually download files.
+    This class provides methods to download data from the Tesouro Direto directly from URLs
+    provided by Tesouro Transparente and save them locally in Parquet format with Snappy compression.
 
     Methods:
-        ler_estoque(): Returns data about the stock of Tesouro Direto.
-        ler_vendas(): Returns data about the sales of Tesouro Direto.
-        ler_taxas(): Returns data about the rates of Tesouro Direto.
-        ler_operacoes(): Returns data about the operations of Tesouro Direto.
-        ler_investidores(): Returns data about the investors of Tesouro Direto.
+        download_all_data(base_filepath): Downloads and saves all datasets in Parquet format.
     """
     def __init__(self):
-        self.url_estoque = "https://www.tesourotransparente.gov.br/ckan/dataset/4d4dac3b-96d2-4011-92c9-ddf7d8392622/resource/650cdc18-0513-4bb1-9222-003ad1c11ac7/download/EstoqueTesouroDireto.csv"
-        self.url_vendas = "https://www.tesourotransparente.gov.br/ckan/dataset/f0468ecc-ae97-4287-89c2-6d8139fb4343/resource/e5f90e3a-8f8d-4895-9c56-4bb2f7877920/download/VendasTesouroDireto.csv"
-        self.url_td_tax = "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv"
-        self.url_td_op = "https://www.tesourotransparente.gov.br/ckan/dataset/78739a33-4d2f-4e35-88fd-65f1ccbe81c4/resource/4100d614-d1ad-4b62-9435-84f7943e46f3/download/OperacoesTesouroDireto.csv"
-        self.url_inves_td = "https://www.tesourotransparente.gov.br/ckan/dataset/48a7fd9d-78e5-43cb-bcba-6e7dcaf2d741/resource/0fd2ac86-4673-46c0-a889-b46224ade563/download/InvestidoresTesouroDireto.csv"
+        self.urls = {
+            "estoque": "https://www.tesourotransparente.gov.br/ckan/dataset/4d4dac3b-96d2-4011-92c9-ddf7d8392622/resource/650cdc18-0513-4bb1-9222-003ad1c11ac7/download/EstoqueTesouroDireto.csv",
+            "vendas": "https://www.tesourotransparente.gov.br/ckan/dataset/f0468ecc-ae97-4287-89c2-6d8139fb4343/resource/e5f90e3a-8f8d-4895-9c56-4bb2f7877920/download/VendasTesouroDireto.csv",
+            "taxas": "https://www.tesourotransparente.gov.br/ckan/dataset/df56aa42-484a-4a59-8184-7676580c81e3/resource/796d2059-14e9-44e3-80c9-2d9e30b405c1/download/PrecoTaxaTesouroDireto.csv",
+            "operacoes": "https://www.tesourotransparente.gov.br/ckan/dataset/78739a33-4d2f-4e35-88fd-65f1ccbe81c4/resource/4100d614-d1ad-4b62-9435-84f7943e46f3/download/OperacoesTesouroDireto.csv",
+            "investidores": "https://www.tesourotransparente.gov.br/ckan/dataset/48a7fd9d-78e5-43cb-bcba-6e7dcaf2d741/resource/0fd2ac86-4673-46c0-a889-b46224ade563/download/InvestidoresTesouroDireto.csv"
+        }
 
-    def read_estoque(self):
+    def download_csv(self, url, filepath):
         """
-        This method reads and returns the stock data of Tesouro Direto from a CSV file.
+        Downloads a CSV file from the given URL and saves it locally.
 
-        The CSV file is read using a specific delimiter and decimal separator. 
-        The date columns are converted to the correct date format.
-
-        Returns:
-            DataFrame: A Pandas DataFrame containing the stock data of Tesouro Direto. 
-            The DataFrame includes columns for the type of title, title maturity date, 
-            stock month, unit price, quantity, and value.
+        Args:
+            url (str): The URL to download the CSV file from.
+            filepath (str): The local path to save the downloaded CSV file.
         """
-        # Reading the CSV file with specific delimiter and decimal separator
-        df = pd.read_csv(self.url_vendas, delimiter=';', decimal=',')
+        response = requests.get(url)
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
 
-        # Converting date columns to the correct date format
-        df['Vencimento do Titulo'] = pd.to_datetime(df['Vencimento do Titulo'], format='%d/%m/%Y')
-        df['Mes Estoque'] = pd.to_datetime(df['Mes Estoque'], format='%d/%m/%Y')
-
-        # Returning the DataFrame
-        return df
-
-    def read_vendas(self):
+    def convert_to_parquet(self, csv_filepath, parquet_filepath, date_columns):
         """
-        This method reads and returns the sales data of Tesouro Direto from a CSV file.
+        Converts a CSV file to Parquet format with Snappy compression.
 
-        The CSV file is read using a specific delimiter and decimal separator. 
-        The date columns are converted to the correct date format.
-
-        Returns:
-            DataFrame: A Pandas DataFrame containing the sales data of Tesouro Direto. 
-            The DataFrame includes columns for the type of title, title maturity date, 
-            sale date, unit price, quantity, and value.
+        Args:
+            csv_filepath (str): The path to the CSV file.
+            parquet_filepath (str): The path to save the Parquet file.
+            date_columns (list): List of columns to parse as dates.
         """
-        # Reading the CSV file with specific delimiter and decimal separator
-        df = pd.read_csv(self.url_vendas, delimiter=';', decimal=',')
+        df = pd.read_csv(csv_filepath, delimiter=';', decimal=',', dayfirst=True, parse_dates=date_columns, date_parser=lambda x: pd.to_datetime(x, format='%d/%m/%Y'))
+        df.to_parquet(parquet_filepath, compression='snappy')
 
-        # Converting date columns to the correct date format
-        df['Vencimento do Titulo'] = pd.to_datetime(df['Vencimento do Titulo'], format='%d/%m/%Y')
-        df['Data Venda'] = pd.to_datetime(df['Data Venda'], format='%d/%m/%Y')
-
-        # Returning the DataFrame
-        return df
-
-    def read_taxas(self):
+    def download_and_convert(self, key, url, base_filepath, date_columns):
         """
-        Reads and returns data about the rates (taxas) of Tesouro Direto.
+        Downloads a CSV file and converts it to Parquet format.
 
-        This method fetches the dataset from the specified URL and loads it into a Pandas DataFrame.
-        The dataset contains information about the rates associated with different securities in the Tesouro Direto program.
-        It handles custom delimiters, decimal formats, and date formats.
-
-        Returns:
-            DataFrame: A Pandas DataFrame containing the rates data of Tesouro Direto.
+        Args:
+            key (str): The dataset key.
+            url (str): The URL to download the CSV file from.
+            base_filepath (str): The base file path to save the Parquet file.
+            date_columns (list): List of columns to parse as dates.
         """
-        # Reading the CSV file with specific delimiter and decimal separator
-        df = pd.read_csv(self.url_td_tax, delimiter=';', decimal=',')
+        csv_filepath = f"{base_filepath}_{key}.csv"
+        parquet_filepath = f"{base_filepath}_{key}.parquet"
+        self.download_csv(url, csv_filepath)
+        self.convert_to_parquet(csv_filepath, parquet_filepath, date_columns)
+        os.remove(csv_filepath)
 
-        # Converting date columns to the correct date format
-        df['Data Vencimento'] = pd.to_datetime(df['Data Vencimento'], format='%d/%m/%Y')
-        df['Data Base'] = pd.to_datetime(df['Data Base'], format='%d/%m/%Y')
-
-        return df
-
-    def read_operacoes(self):
+    def download_all_data(self, base_filepath):
         """
-        Reads and returns data about the operations (operacoes) of Tesouro Direto.
+        Downloads and saves all datasets in Parquet format in parallel.
 
-        This method accesses the operations dataset, detailing the various transactions
-        and operations carried out in the Tesouro Direto program. The data is loaded
-        into a Pandas DataFrame directly from the provided URL.
-
-        Returns:
-            DataFrame: A Pandas DataFrame containing the operations data of Tesouro Direto.
+        Args:
+            base_filepath (str): The base file path to save the Parquet files.
         """
-        return pd.read_csv(self.url_td_op)
+        date_columns = {
+            "estoque": ['Vencimento do Titulo', 'Mes Estoque'],
+            "vendas": ['Vencimento do Titulo', 'Data Venda'],
+            "taxas": ['Data Vencimento', 'Data Base'],
+            "operacoes": [],
+            "investidores": []
+        }
 
-    def read_investidores(self):
-        """
-        Reads and returns data about the investors (investidores) of Tesouro Direto.
-
-        This method retrieves data regarding the investors in the Tesouro Direto program.
-        The dataset includes demographic and other relevant information about the investors,
-        and is loaded into a Pandas DataFrame from the specified URL.
-
-        Returns:
-            DataFrame: A Pandas DataFrame containing the investors data of Tesouro Direto.
-        """
-        return pd.read_csv(self.url_inves_td)
+        tasks = []
+        with ThreadPoolExecutor() as executor:
+            for key, url in self.urls.items():
+                tasks.append(executor.submit(self.download_and_convert, key, url, base_filepath, date_columns[key]))
+            
+            for _ in tqdm(tasks, desc="Downloading and converting datasets"):
+                _ = [task.result() for task in tasks]
